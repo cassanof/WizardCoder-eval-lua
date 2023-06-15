d630f566f9f8e510175e55bbfc8fcbdc4fca86fa
cassanof <federico.cassano@federico.codes>
2023-06-15 13:11:44 -0700

    aa

--- WizardCoder/src/gen.sh
+++ WizardCoder/src/gen.sh
@@ -10,11 +10,6 @@ mkdir -p ${output_path}
 echo 'Output path: '$output_path
 echo 'Model to eval: '$model
 
-num_problems=164
-
-for i in $(seq 0 1 $((num_problems-1)))
-do
-    CUDA_VISIBLE_DEVICES=$gpu python humaneval_gen.py --model ${model} \
-      --start_index ${start_index} --end_index ${end_index} --temperature ${temp} \
-      --num_seqs_per_iter ${num_seqs_per_iter} --N ${pred_num} --max_len ${max_len} --output_path ${output_path};
-done
+python humaneval_gen.py --model ${model} \
+  --start_index 0 --end_index 163 --temperature ${temp} \
+  --num_seqs_per_iter ${num_seqs_per_iter} --N ${pred_num} --max_len ${max_len} --output_path ${output_path};
