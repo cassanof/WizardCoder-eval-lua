e912cde67bb0b6b31b86f3d615c1e6bd6922a45a
cassanof <federico.cassano@federico.codes>
2023-06-15 13:09:58 -0700

    a

--- WizardCoder/src/gen.sh
+++ WizardCoder/src/gen.sh
@@ -10,20 +10,10 @@ mkdir -p ${output_path}
 echo 'Output path: '$output_path
 echo 'Model to eval: '$model
 
-# 164 problems, 21 per GPU if GPU=8
-index=0
-gpu_num=1
-for ((i = 0; i < $gpu_num; i++)); do
-  start_index=$((i * 21))
-  end_index=$(((i + 1) * 21))
+num_problems=164
 
-  gpu=$((i))
-  echo 'Running process #' ${i} 'from' $start_index 'to' $end_index 'on GPU' ${gpu}
-  ((index++))
-  (
+for i in $(seq 0 1 $((num_problems-1)))
     CUDA_VISIBLE_DEVICES=$gpu python humaneval_gen.py --model ${model} \
       --start_index ${start_index} --end_index ${end_index} --temperature ${temp} \
       --num_seqs_per_iter ${num_seqs_per_iter} --N ${pred_num} --max_len ${max_len} --output_path ${output_path}
-  ) &
-  if (($index % $gpu_num == 0)); then wait; fi
 done
